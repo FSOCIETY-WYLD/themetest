f7e724a80525abbac46bef7bd42ef85a9cba0a00
FSOCIETY-WYLD <121406222+FSOCIETY-WYLD@users.noreply.github.com>
2023-10-06 12:29:01 +0200

    Create main.lua

---
+++ main.lua
@@ -0,0 +1,8 @@
+local Custom = {
+    Main = Color3.fromRGB(158, 55, 158),
+    Second = Color3.fromRGB(53, 0, 101),
+    Stroke = Color3.fromRGB(71, 37, 126),
+    Divider = Color3.fromRGB(71, 37, 126),
+    Text = Color3.fromRGB(255, 255, 255),
+    TextDark = Color3.fromRGB(150, 150, 150)
+}
